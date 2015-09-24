ba383bc24d441dd83a2457cd04ec9619e2416119
Gigabit101 <thegigabit101@gmail.com>
2015-09-24 17:51:08 +0100

    Update EasyBreeding.java

--- src/main/java/easyBreeding/EasyBreeding.java
+++ src/main/java/easyBreeding/EasyBreeding.java
@@ -4,7 +4,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid="easyBreeding", name="Easy Breeding", version="2.0.2", acceptableRemoteVersions = "*")
+@Mod(modid="easyBreeding", name="Easy Breeding", version="2.0.3", acceptableRemoteVersions = "*")
 public class EasyBreeding 
 {
 	@Mod.EventHandler
